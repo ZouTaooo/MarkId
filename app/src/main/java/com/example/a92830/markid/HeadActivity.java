f9bf077f1d9983c34867772dbfe28fec46869565
ZouTao <928302279@qq.com>
2017-07-10 10:47:14 +0800

    创建仓库

---
+++ app/src/main/java/com/example/a92830/markid/HeadActivity.java
@@ -0,0 +1,13 @@
+package com.example.a92830.markid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HeadActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_head);
+    }
+}
